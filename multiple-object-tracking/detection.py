e78babce733722286cfdac2b1f26c1aab12aef88
FredJiang0324 <113407918+FredJiang0324@users.noreply.github.com>
2022-12-09 16:03:08 -0600

    Update detection.py

--- multiple-object-tracking/detection.py
+++ multiple-object-tracking/detection.py
@@ -4,11 +4,6 @@ import numpy as np
 
 
 def load_det(file_dir):
-    """
-    根据file目录，产生观测list
-    :param file_dir: 每帧观测分别对应一个txt文件，每个文件中多个目标观测逐行写入
-    :return: 所有观测list，[[帧1所有观测],[帧2所有观测]]
-    """
     det_list = []
     for index in range(len(os.listdir(file_dir))):
         List = []
